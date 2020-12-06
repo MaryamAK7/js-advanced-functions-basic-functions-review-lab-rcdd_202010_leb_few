9463f6007aee08e1c1cf88a6d3c432f03932ea3b
Maryam Abdel-karim <maryam.abdelkarim@hotmail.com>
2020-12-06 17:56:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Your code here
 function saturdayFun(activity){
   if(activity==="")
-  return "This Saturday, I want to "+s+"!";
+  return "This Saturday, I want to roller-skate!";
   else return "This Saturday, I want to "+activity+"!";
   
 }
